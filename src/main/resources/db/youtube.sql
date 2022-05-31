b42c4da188f3356032c8d2bef5756b159cea6099
rjs595959 <rjs595959@naver.com>
2022-06-01 07:51:14 +0900

    @fix publishedAt type을 datetime으로 수정

--- src/main/resources/db/youtube.sql
+++ src/main/resources/db/youtube.sql
@@ -3,12 +3,6 @@ CREATE TABLE `youtube` (
   `thumbnailUrl` varchar(50) NOT NULL,
   `title` varchar(100) NOT NULL,
   `description` varchar(500) NOT NULL,
-  `publishedAt` date NOT NULL,
+  `publishedAt` datetime NOT NULL,
   PRIMARY KEY (`videoUrl`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-insert into youtube(videoUrl, thumbnailUrl, title, description, publishedAt) values('1','1','1','1','1');
-insert into youtube(videoUrl, thumbnailUrl, title, description, publishedAt) values('2','1','1','1','1');
-insert into youtube(videoUrl, thumbnailUrl, title, description, publishedAt) values('3','1','1','1','1');
-insert into youtube(videoUrl, thumbnailUrl, title, description, publishedAt) values('4','1','1','1','1');
-insert into youtube(videoUrl, thumbnailUrl, title, description, publishedAt) values('5','1','1','1','1');

d127db6c10e84bb7d7461d6fa5cb5e149c154245
JangDongGun <rjs595959@github.com>
2022-06-04 15:48:46 +0900

    @refactor youtubeDAO 코드 리팩토링(YoutubeListNum 제거)

--- src/main/java/com/spring/ex/dao/YoutubeDAO.java
+++ src/main/java/com/spring/ex/dao/YoutubeDAO.java
@@ -9,7 +9,6 @@ import com.spring.ex.dto.YoutubeDTO;
 
 @Repository
 public interface YoutubeDAO {
-	public int youtubeListNum();
 	public List<YoutubeDTO> youtubeList(Integer start, Integer bound);
 	public void insertYoutubeList(HashMap<String, String> hashMap);
 }
